fc187109f9b69054cef4f72e7bc1d8299252e187
unknown <luc1497@gmail.com>
2025-03-26 21:28:11 -0300

    att

--- src/app/app/(pages)/dashboard/components/quickRegister/cardData.tsx
+++ src/app/app/(pages)/dashboard/components/quickRegister/cardData.tsx
@@ -5,9 +5,11 @@ import { useState } from "react";
 import formatarEmReal from "@/app/app/utils/formatarEmReal";
 import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { LoaderCircle, Plus, Check } from "lucide-react";
+import { LoaderCircle, Plus, Check, UsersIcon } from "lucide-react";
 import axios from "axios";
 import { useSession } from "next-auth/react";
+import { Input } from "@/components/ui/input";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 interface Service {
   id: string;
@@ -35,15 +37,24 @@ interface CardDataProps {
 }
 
 export function CardData({ services, setIsSaved, isSaved, paymentMethods }: CardDataProps) {
-  const [serviceToSave, setServiceToSave] = useState<Service[]>([]);
+  const [serviceToSave, setServiceToSave] = useState<Service[]>([
+    {
+      id: "",
+      name: "",
+      value: 0
+    }
+  ]);
   const [paymentMethodToSave, setPaymentMethodsToSave] = useState<string | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(false);
   const { data: session } = useSession();
+  const [selectedCustomer, setSelectedCustomer] = useState(undefined)
+  const [customers, setCustomers] = useState(undefined)
 
   const handleAddSelect = () => {
     setServiceToSave((prev) => [...prev, { id: "", name: "", value: 0 }]);
   };
 
+  
 
 
   const handleChangeService = (index: number, serviceId: string) => {
@@ -84,54 +95,103 @@ export function CardData({ services, setIsSaved, isSaved, paymentMethods }: Card
       <Label className="pb-1">Serviços realizados:</Label>
       {serviceToSave.map((selected, index) => (
         <div key={index} className="flex items-center gap-2">
-          <Select value={selected.id} onValueChange={(value) => handleChangeService(index, value)}>
+          <Select
+            value={selected.id}
+            onValueChange={(value) => handleChangeService(index, value)}
+          >
             <SelectTrigger>
               <SelectValue placeholder={selected.name || "Selecione"} />
             </SelectTrigger>
             <SelectContent>
               {services
-                .filter((s) => !serviceToSave.some((sel) => sel.id === s.id) || s.id === selected.id)
+                .filter(
+                  (s) =>
+                    !serviceToSave.some((sel) => sel.id === s.id) ||
+                    s.id === selected.id
+                )
                 .map((service) => (
-                  <SelectItem className="hover:cursor-pointer" key={service.id} value={service.id}>
+                  <SelectItem
+                    className="hover:cursor-pointer"
+                    key={service.id}
+                    value={service.id}
+                  >
                     {service.name} - {formatarEmReal(service.value)}
                   </SelectItem>
                 ))}
             </SelectContent>
           </Select>
-          <Button className="rounded-full size-7 bg-red-700" onClick={() => handleRemoveService(index)}>
+          <Button
+            className="rounded-full size-7 bg-red-700"
+            onClick={() => handleRemoveService(index)}
+          >
             ✕
           </Button>
         </div>
       ))}
-        
+
       <div className="flex w-full justify-center mb-8">
         <Button className="rounded-full size-8" onClick={handleAddSelect}>
           <Plus />
         </Button>
       </div>
-        <Select onValueChange={(value) => setPaymentMethodsToSave(value)}>
-          <SelectTrigger>
-            <SelectValue placeholder="Forma de pagamento" />
-          </SelectTrigger>
-          <SelectContent>
-            {paymentMethods.map((paymentMethod)=>(
-            <SelectItem className="hover:cursor-pointer" key={paymentMethod.id} value={paymentMethod.id}>
+      <Label>Cliente: </Label>
+      <div className="w-full flex flex-row items-center gap-3">
+        <Input
+          disabled
+          value={selectedCustomer}
+          placeholder="Selecione o cliente"
+          type="text"
+        ></Input>
+        <Dialog>
+          <DialogTrigger asChild>
+        <Button className="hover: cursor-pointer">
+          <UsersIcon />
+        </Button>
+
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Clientes</DialogTitle>
+              <DialogDescription>Selecione um cliente para o serviço</DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col items-center justify-center">
+              
+            <Input placeholder="Buscar" className="w-full"></Input>
+      
+            </div>
+          </DialogContent>
+
+        </Dialog>
+      </div>
+      <Select onValueChange={(value) => setPaymentMethodsToSave(value)}>
+        <SelectTrigger>
+          <SelectValue placeholder="Forma de pagamento" />
+        </SelectTrigger>
+        <SelectContent>
+          {paymentMethods.map((paymentMethod) => (
+            <SelectItem
+              className="hover:cursor-pointer"
+              key={paymentMethod.id}
+              value={paymentMethod.id}
+            >
               {paymentMethod.name} - {paymentMethod.bankAccount.bankName}
             </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>        
-      <ul>
+          ))}
+        </SelectContent>
+      </Select>
+      {/* <ul>
         {serviceToSave.map((service) => (
           <li key={service.id} className="flex justify-between text-xs">
             {service.name} - {formatarEmReal(service.value)}
           </li>
         ))}
-      </ul>
+      </ul> */}
       <Separator className="my-4" />
       <div className="flex gap-2 items-center">
         <Label>Total:</Label>
-        <Label className="text-md">{formatarEmReal(serviceToSave.reduce((acc, s) => acc + s.value, 0))}</Label>
+        <Label className="text-md">
+          {formatarEmReal(serviceToSave.reduce((acc, s) => acc + s.value, 0))}
+        </Label>
         <div className="w-full"></div>
         {isSaved ? (
           <Button disabled>
